622fb060bf67144781116a716a1a529494dc3bac
balcoder <des.barrett@gmail.com>
2026-02-26 11:14:01 +0000

    first commit

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+console.log('test 1');
+console.log('test 1.1');
+const img1 = document.getElementById('foo');
+const img = new Image();
+img.src = "./img/hotel-1.jpg";
+img.setAttribute('width', '50%');
+img.setAttribute('height', '50%');
+img1.appendChild(img);
+
+console.log('test 2');
+console.log('test 2.1');
+console.log("added new file");
