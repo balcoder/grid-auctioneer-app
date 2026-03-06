7e58ef835828e4e583f8b64322bf4e9f7f0b4549
balcoder <des.barrett@gmail.com>
2026-03-06 11:59:37 +0000

    features section done

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-console.log('test 1');
-console.log('test 1.1');
-const img1 = document.getElementById('foo');
-const img = new Image();
-img.src = "./img/hotel-1.jpg";
-img.setAttribute('width', '50%');
-img.setAttribute('height', '50%');
-img1.appendChild(img);
